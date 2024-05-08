7a786b8b3a45a114d45bbf3a92f1e219982d3095
kosm0ssy <xzerokim@gmail.com>
2024-05-08 16:43:56 +0900

    Division
    division upload

--- src/test/java/com/ohgiraffers/calc/DivisionTests.java
+++ src/test/java/com/ohgiraffers/calc/DivisionTests.java
@@ -7,12 +7,22 @@ import org.junit.jupiter.api.Test;
 public class DivisionTests {
 
     @Test
-    @DisplayName("4와 2를 전달했을 때 2가 나오는지 확인")
+    @DisplayName("4와 2를 전달했을 때 2가 나오는 지 확인")
     public void testDivision() {
 
         Division division = new Division();
         int result = division.divideTwoNumbers(4,2);
 
         Assertions.assertEquals(2, result);
+    }
+
+    @Test
+    @DisplayName("6과 1을 전달했을 때 6이 나오는 지 확인")
+    public void testDivision1() {
+
+        Division division = new Division();
+        int result = division.divideTwoNumbers(6,1);
+
+        Assertions.assertEquals(6, result);
     }
 }
