bdb38e429deed43b2f02bdcd63c9c0e991a4971f
kosm0ssy <xzerokim@gmail.com>
2024-05-08 16:38:04 +0900

    Division
    division upload

--- src/main/java/com/ohgiraffers/calc/Application.java
+++ src/main/java/com/ohgiraffers/calc/Application.java
@@ -4,8 +4,7 @@ public class Application {
     public static void main(String[] args) {
 
 
-        Division adder = new Division();
-        int result = adder.divideTwoNumbers(2,3);
+
 
     }
 }
